4e82f6390d0d9c8e38b01c0a4972c815ca3d31f0
Nasim Imtiaz <38372664+Nasim-Imtiaz@users.noreply.github.com>
2019-11-25 15:11:57 -0800

    NavigationView

---
+++ App02/App02/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  App02
+//
+//  Created by user163222 on 11/25/19.
+//  Copyright © 2019 user163222. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack{
+                HStack(spacing: 0.0){
+                    Text("U")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                    Text("Funding")
+                        .font(.largeTitle)
+                }.padding()
+                
+                Text("Online Fundraising Made First")
+                
+                HStack(spacing: 20){
+                    NavigationLink(destination: LoginView()){
+                         Text("Sign Up")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                    }
+                    NavigationLink(destination: LoginView()){
+                         Text("Login")
+                         .foregroundColor(.black)
+                         .frame(maxWidth: .infinity)
+                         .padding()
+                         .background(Color.white)
+                         .border(Color.black)
+                    }
+                }.padding([.top, .leading, .trailing, .bottom],30)
+            }
+        }.statusBar(hidden:true)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ App02/App02/LoginView.swift
@@ -0,0 +1,40 @@
+//
+//  LoginView.swift
+//  App02
+//
+//  Created by user163222 on 11/25/19.
+//  Copyright © 2019 user163222. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    init(){
+        UITableView.appearance().backgroundColor = .clear
+    }
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    var body: some View {
+       NavigationView {
+            Form {
+                HStack(alignment:.center){
+                    TextField("Enter Your Mail", text: $email).padding()
+                }
+                TextField("Enter Your Password", text: $password).padding()
+                Button(action:{print(self.$email)}){
+                    Text("Login")
+                }
+            }
+            .navigationBarTitle(Text("Login Form"))
+            
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
